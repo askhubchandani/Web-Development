6643eeed8617ff27eba7cc545f75e392dae0e895
Adarsh Khubchandani <askhubchandani@users.noreply.github.com>
2018-03-25 18:58:30 +0530

    Update script.js

--- Day-30/script.js
+++ Day-30/script.js
@@ -1,1 +1,39 @@
+/* Apps List Toggle */
 
+$(document).ready(function(){
+    $(".apps-list").click(function(){
+        $("#apps").toggle();
+    });
+});
+
+/* End Apps List Toggle */
+
+/* Notifications Toggle */
+
+$(document).ready(function(){
+    $(".notification-btn").click(function(){
+        $("#notif").toggle();
+    });
+});
+
+/* End Notifications Toggle */
+
+/* User Account Toggle */
+
+$(document).ready(function(){
+    $(".user-acc-btn").click(function(){
+        $("#user").toggle();
+    });
+});
+
+/* End User Account Toggle */
+
+/* Settings Toggle */
+
+$(document).ready(function(){
+    $(".settings-btn").click(function(){
+        $("#settings").toggle();
+    });
+});
+
+/* End Settings Toggle */
